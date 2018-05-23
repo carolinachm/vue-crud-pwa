0203542afaddec6c14d17775c4fed0366e9f8e23
Carolina Mesquita <carolti2013@gmail.com>
2018-05-22 23:07:29 -0300

    templete

---
+++ vue-crud-pwa/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+      :clipped-left="clipped"
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+     
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-navigation-drawer
+      temporary
+      :right="right"
+      v-model="rightDrawer"
+      fixed
+      app
+    >
+      
+    </v-navigation-drawer>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    
+      items: [
+        {
+          icon: "home",
+          title: "Inicio"
+        },
+        {
+          icon: "face",
+          title: "Cliente"
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Crud Cliente"
+    };
+  },
+  name: "App"
+};
+</script>
---
+++ vue-crud-pwa/src/components/Cliente.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex lg4 md6 sm10 xs12>
+        <v-card>
+          <v-card-text>Cliente</v-card-text>
+        </v-card>
+      </v-flex>
+     
+    </v-layout>
+  </v-container>
